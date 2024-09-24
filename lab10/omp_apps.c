ae65fb3ade8817e884dbeca239e86ee61bdacd1a
Wang Yanjie <wang_yanjie@Wangs-MacBook-Air.local>
2024-09-24 21:25:12 +0800

    lab10 dot product

--- lab10/omp_apps.c
+++ lab10/omp_apps.c
@@ -83,11 +83,14 @@ double dotp_manual_optimized(double* x, double* y, int arr_size) {
   // Do NOT use the `reduction` directive here!
   double global_sum = 0.0;
   #pragma omp parallel
-  {
+  { 
+    double local_sum = 0.0;
     #pragma omp for
-    for (int i = 0; i < arr_size; i++)
-      #pragma omp critical
-      global_sum += x[i] * y[i];
+    for (int i = 0; i < arr_size; i++) {
+      local_sum += x[i] * y[i];
+    }
+    #pragma omp critical
+    global_sum += local_sum;
   }
   return global_sum;
 }
@@ -97,11 +100,9 @@ double dotp_reduction_optimized(double* x, double* y, int arr_size) {
   // TODO: Modify this function
   // Please DO use the `reduction` directive here!
   double global_sum = 0.0;
-  #pragma omp parallel
+  #pragma omp parallel for reduction(+:global_sum)
   {
-    #pragma omp for
     for (int i = 0; i < arr_size; i++)
-      #pragma omp critical
       global_sum += x[i] * y[i];
   }
   return global_sum;
